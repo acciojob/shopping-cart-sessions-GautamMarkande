003fd2b7cea8a7cc5eaf99499e66cceb15b534de
GautamMarkande <118103083+GautamMarkande@users.noreply.github.com>
2024-04-02 17:24:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,7 +10,9 @@ const products = [
 ];
 
 // DOM elements
+const clrCartBtn = document.getElementById("clear-cart-btn")
 const productList = document.getElementById("product-list");
+const cartList = document.getElementById("cart-list");
 
 // Render product list
 function renderProducts() {
@@ -20,26 +22,85 @@ function renderProducts() {
     productList.appendChild(li);
   });
 }
-
+renderProducts()
 
 // Render cart list
-function renderCart() {}
+function renderCart() {
+	cartList.innerHTML="";
+	let arr=[]
+	if(sessionStorage.getItem("data")){
+		const sessionArr = JSON.parse( sessionStorage.getItem("data"))
+		arr = [...sessionArr];
+		console.log(arr)
+		// sessionStorage.setItem("data", JSON.stringify(arr));
+	}
+	products.forEach((product)=>{
+		if(arr.find((id)=>id===product.id.toString())){
+			 const li = document.createElement("li");
+		    li.innerHTML = `${product.name} - $${product.price} <button class="add-to-cart-btn" data-id="${product.id}" onclick="removeFromCart(${product.id})">remove from Cart</button>`;
+		    cartList.appendChild(li);
+		}
+	})
+}
 
 // Add item to cart
-const addcardbtn = document.querySelector(".add-to-cart-btn")
-addcardbtn.addEventListener("click",addToCart(e.getAttribute("data-id")))
-function addToCart(productId) {
-	for (let product of products) {
-		sessionStorage.setItem(productId, JSON.stringify(product));
+const addcardbtn = document.querySelectorAll(".add-to-cart-btn")
+for (let index = 0; index < addcardbtn.length; index++) {
+addcardbtn[index].addEventListener("click",(e)=>{
+	addToCart(e.target.getAttribute("data-id"),e)
+	// cartList.innerHTML="";
+	renderCart()
+})
+}
+function addToCart(productId,e) {
+	
+console.log("i am from addto cart" ,e.target)
+
+	sessionStorage.setItem('myData', "dataString");
+	let arr=[]
+	if(sessionStorage.getItem("data")){
+		var sessionArr = JSON.parse(sessionStorage.getItem("data"))
+		if(!sessionArr.find((id)=>id===productId.toString())){
+		arr = [...sessionArr,productId];
+		}else{
+			arr=[...sessionArr];
+		}
+		sessionStorage.setItem("data", JSON.stringify(arr));
+		console.log(arr)
+		
+	}else{
+		sessionStorage.setItem("data", JSON.stringify(arr));
 	}
+	
 }
 
 // Remove item from cart
-function removeFromCart(productId) {}
+function removeFromCart(productId) {
+	console.log(productId)
+	let arr = [];
+	if(sessionStorage.getItem("data")){
+		const sessionArr = JSON.parse(sessionStorage.getItem("data"))
+		arr=sessionArr.filter((id)=>{
+				return id!==productId.toString()
+		})
+		sessionStorage.setItem("data", JSON.stringify(arr));
+	}
+	console.log(arr)
+	renderCart()
+}
 
 // Clear cart
-function clearCart() {}
+clrCartBtn.addEventListener('click',()=>{
+	console.log("ok")
+	clearCart()
+   renderCart();
+})
+function clearCart() {
+	
+	let arr=[]
+	sessionStorage.setItem("data", JSON.stringify(arr));
+}
 
 // Initial render
-renderProducts();
+
 renderCart();
